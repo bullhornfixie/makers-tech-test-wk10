d0fed230fac20981128786adb1ade6bc107a49b9
bullhornfixie <dawson.jon@hotmail.co.com>
2021-03-10 07:42:59 +0000

    aligned statement columns with whitespace in debit and credit column if nil

--- src/BankAccount.js
+++ src/BankAccount.js
@@ -10,14 +10,14 @@ class BankAccount {
 BankAccount.prototype.deposit = function(amount) {
   var toFloat = amount.toFixed(2)
   var update = this.balance += amount
-  this.history.push([`${this.getDateToday()} || ${toFloat} ||  'nil' || ${update.toFixed(2)}\n`])
-}
+  this.history.push([`${this.getDateToday()} || ${toFloat} ||        || ${update.toFixed(2)}\n`])
+};
 
 BankAccount.prototype.withdraw = function(amount) {
   var toFloat = amount.toFixed(2)
   var update = this.balance -= amount
-  this.history.push([`${this.getDateToday()} ||  'nil' || ${toFloat} || ${update.toFixed(2)}\n`])
-}
+  this.history.push([`${this.getDateToday()} ||        || ${toFloat} || ${update.toFixed(2)}\n`])
+};
 
 BankAccount.prototype.getDateToday = function () {
   var today = new Date();
@@ -25,19 +25,22 @@ BankAccount.prototype.getDateToday = function () {
   var mm = String(today.getMonth() + 1).padStart(2, '0');
   var yyyy = today.getFullYear();
 
-  today = dd + '/' + mm + '/' + yyyy;
-  return today
-}
+    today = dd + '/' + mm + '/' + yyyy;
+    return today
+};
 
 BankAccount.prototype.statement = function () {
   var header = 'date       || credit || debit  || balance'
+  var balance = this.balance
   var arr = this.history.reverse()
-  if (arr.length > 0) {
-    console.log(header + '\n' + arr.join(''))
-  } else {
-    this.history.push([`${this.getDateToday()} ||  'nil' ||  'nil' || ${Number(0).toFixed(2)}\n`])
-    console.log(header + '\n' + arr.join(''))
-  }
-}
+
+    if (arr.length > 0) {
+      console.log(header + '\n' + arr.join(''))
+    } else {
+      this.history.push([`${this.getDateToday()} ||        ||        || ${balance.toFixed(2)}\n`])
+  
+      console.log(header + '\n' + arr.join(''))
+    }
+};
 
 
